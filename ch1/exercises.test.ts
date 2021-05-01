7857b7001a770480b4cb0c778db9d6e9155f5010
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-05-01 10:22:21 -0400

    add strings and arrays up to Q5

--- ch1/exercises.test.ts
+++ ch1/exercises.test.ts
@@ -1,5 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
-import { Q1_isUnique, Q2_checkPermutation } from "./exercises.ts";
+import {
+  Q1_isUnique,
+  Q2_checkPermutation,
+  Q3_URLify,
+  Q4_palindromePermutations,
+  Q5_oneAway
+} from "./exercises.ts";
 
 Deno.test("Q1: Is Unique", () => {
   assertEquals(Q1_isUnique("abcdef"), true);
@@ -11,3 +17,22 @@ Deno.test("Q2: Check Permutations", () => {
   assertEquals(Q2_checkPermutation("babababa", "foo"), false);
   assertEquals(Q2_checkPermutation("racecar", "care"), true);
 });
+
+Deno.test("Q3: URLify", () => {
+  assertEquals(Q3_URLify("hello world", 11), "hello%20world");
+  assertEquals(Q3_URLify("hello world     ", 11), "hello%20world");
+  assertEquals(Q3_URLify("hello world     ", 12), "hello%20world%20");
+});
+
+Deno.test("Q4: Palindrome Permutation", () => {
+  assertEquals(Q4_palindromePermutations("Tact coa"), true);
+  assertEquals(Q4_palindromePermutations("cra e   rac"), true);
+  assertEquals(Q4_palindromePermutations("aaaaae"), false);
+});
+
+Deno.test('Q5: One Away', () => {
+  assertEquals(Q5_oneAway('pale', 'ple'), true);
+  assertEquals(Q5_oneAway('pales', 'pale'), true);
+  assertEquals(Q5_oneAway('pale', 'bale'), true);
+  assertEquals(Q5_oneAway('pale', 'bake'), false);
+})
--- ch1/exercises.ts
+++ ch1/exercises.ts
@@ -20,3 +20,93 @@ export function Q2_checkPermutation(a: string, b: string): boolean {
   }
   return true;
 }
+
+export function Q3_URLify(str: string, length: number) {
+  // convert str to char array
+  const strArr = str.split("");
+
+  // delete excess characters
+  strArr.splice(length);
+
+  // replace spaces with %20
+  let i = 0;
+  while (i < strArr.length) {
+    if (strArr[i] === " ") {
+      strArr.splice(i, 1, "%", "2", "0");
+      i += 3;
+    } else {
+      i++;
+    }
+  }
+
+  return strArr.join("");
+}
+
+export function Q4_palindromePermutations(_str: string): boolean {
+  const str = _str.toLowerCase();
+  const firstCharCode = "a".charCodeAt(0);
+  const lastCharCode = "z".charCodeAt(0);
+
+  const uniq = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    if (charCode < firstCharCode || charCode > lastCharCode) continue;
+    if (uniq.has(str[i])) {
+      uniq.delete(str[i]);
+    } else {
+      uniq.add(str[i]);
+    }
+  }
+
+  return uniq.size === 0 || uniq.size === 1;
+}
+
+export function Q5_oneAway(a: string, b: string): boolean {
+  function oneAway(a: string, b: string): boolean {
+    if (a.length === b.length) {
+      return isOneReplaceAway(a, b);
+    } else if (a.length === b.length + 1) {
+      return isOneRemoveAway(a, b);
+    } else if (b.length === a.length + 1) {
+      return isOneRemoveAway(b, a);
+    }
+    return false;
+  }
+
+  function isOneReplaceAway(a: string, b: string): boolean {
+    let hasReplaced = false;
+    for (let i = 0; i < a.length; i++) {
+      const charA = a[i];
+      const charB = b[i];
+
+      if (charA === charB) continue;
+
+      if (hasReplaced) return false;
+
+      hasReplaced = true;
+    }
+    return true;
+  }
+
+  function isOneRemoveAway(longer: string, shorter: string): boolean {
+    let offset = 0;
+
+    for (let i = 0; i < shorter.length; i++) {
+      const charS = shorter[i];
+      const charL = longer[i + offset];
+
+      if (charL === charS) continue;
+
+      if (offset) return false;
+
+      if (charS !== longer[i + 1]) return false;
+
+      offset += 1;
+    }
+
+    return true;
+  }
+
+  return oneAway(a, b);
+}
