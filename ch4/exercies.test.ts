6600bedd5359db57900f351e26315ea6f1fe623f
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-08-08 18:12:42 -0400

    add graphFromString

--- ch4/exercies.test.ts
+++ ch4/exercies.test.ts
@@ -1,7 +1,7 @@
 import * as asserts from "https://deno.land/std@0.95.0/testing/asserts.ts";
-import { NodeBinary } from './exercies.ts'
+import { NodeBinary, NodeN, graphFromString } from "./exercies.ts";
 
-Deno.test('BinaryNode', () => {
+Deno.test("BinaryNode", () => {
   const data = [...Array(20)].map(() => Math.floor(Math.random() * 100));
   const root = new NodeBinary(50);
 
@@ -14,4 +14,36 @@ Deno.test('BinaryNode', () => {
   }
 
   root.printInOrder();
-})
+});
+
+Deno.test("graphFromString - directed", () => {
+  const graphStr = `
+    A > B
+    B > C
+    A > C
+    D > C
+  `;
+
+  const graph = graphFromString(graphStr);
+
+  const root = graph.get("A");
+
+  asserts.assertEquals(root?.children.includes(graph.get("C")), true);
+  asserts.assertEquals(root?.children.includes(graph.get("D")), false);
+  asserts.assertEquals(graph.get("D")?.children.includes(graph.get("C")), true);
+});
+
+Deno.test("graphFromString - undirected", () => {
+  const graphStr = `
+    B <> D
+    C <> D
+    A <> C
+  `;
+
+  const graph = graphFromString(graphStr);
+
+  console.log(graph);
+
+  asserts.assertEquals(graph.get("C")?.children.includes(graph.get("D")), true);
+  asserts.assertEquals(graph.get("D")?.children.includes(graph.get("C")), true);
+});
--- ch4/exercies.ts
+++ ch4/exercies.ts
@@ -1,8 +1,8 @@
 export class NodeN {
   name: string;
-  children: NodeN[];
+  children: any[];
 
-  constructor(name: string, children?: NodeN[]) {
+  constructor(name: string, children?: any[]) {
     this.name = name;
     this.children = children || [];
   }
@@ -55,3 +55,42 @@ export class NodeBinary {
 // Complete binary tree: Every level is filled. The last level is filled as much as possible, from left to right.
 // Full binary tree: Every node has either zero or two child nodes.
 // Perfect binary tree: Every node has 2 children, all leaf nodes are on the same level.
+
+export function graphFromString(input: string): Map<string, NodeN> {
+  /**
+   * Here, we expect a string representation of our graph.
+   * A relation is represented as such: A > B (directed) or A <> B (undirected)
+   */
+
+  const nodes = new Map<string, NodeN>();
+
+  for (let line of input.trim().split("\n")) {
+    const [a, rel, b] = line.trim().split(" ");
+
+    if (!nodes.has(a)) {
+      nodes.set(a, new NodeN(a));
+    }
+
+    if (!nodes.has(b)) {
+      nodes.set(b, new NodeN(b));
+    }
+
+    const nodeA = nodes.get(a);
+    const nodeB = nodes.get(b);
+
+    if (!nodeA || !nodeB) continue;
+
+    if (rel === ">") {
+      nodeA.children.push(nodeB);
+    } else if (rel === "<>") {
+      nodeA.children.push(nodeB);
+      nodeB.children.push(nodeA);
+    } else {
+      throw new Error(
+        `Unexpected relation type ${rel}, must be either > or <>`
+      );
+    }
+  }
+
+  return nodes;
+}
