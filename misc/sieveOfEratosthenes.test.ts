9cdef6ec47e517850f95ff5c863242158e21a4ac
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-09-03 08:59:15 -0400

    add tests for sieve

--- misc/sieveOfEratosthenes.test.ts
+++ misc/sieveOfEratosthenes.test.ts
@@ -1,7 +1,7 @@
 import * as asserts from "https://deno.land/std@0.95.0/testing/asserts.ts";
-import { getPrimeFlags } from './sieveOfEratosthenes.ts'
+import { getPrimeFlags } from "./sieveOfEratosthenes.ts";
 
-Deno.test('Sieve of Eratosthenes', () => {
+Deno.test("Sieve of Eratosthenes", () => {
   const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
   const flags = getPrimeFlags(30);
 
@@ -9,4 +9,4 @@ Deno.test('Sieve of Eratosthenes', () => {
     const expected = primes.includes(i);
     asserts.assertEquals(flags[i], expected);
   }
-})
+});
